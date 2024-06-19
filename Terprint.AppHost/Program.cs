a564e8b65cbdf5809dc13bfe2f64ea871aed32a4
Jamieson Gill <jgill@savitas.net>
2024-06-19 12:48:29 -0400

    Add project files.

---
+++ Terprint.AppHost/Program.cs
@@ -0,0 +1,9 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.Terprint_ApiService>("apiservice");
+
+builder.AddProject<Projects.Terprint_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService);
+
+builder.Build().Run();
