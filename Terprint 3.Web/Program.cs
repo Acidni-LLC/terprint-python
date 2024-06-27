4130644165437e6cf58d0ae1b651313d794a114d
Jamieson Gill <jgill@savitas.net>
2024-06-27 09:09:37 -0400

    changes

--- Terprint 3.Web/Program.cs
+++ Terprint 3.Web/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using System.Configuration;
 //using Terprint_3.Pages;
 //using Terprint_3.Components;
 
@@ -13,7 +18,16 @@ builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsigh
 {
     ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
 });
+//builder.Services.AddAuthentication(AzureADDefaults.AuthenticationScheme)             .AddAzureAD(options => Configuration.Bind("AzureAd", options));
 
+//builder.Services.AddRazorPages().AddMvcOptions(options =>
+//{
+//    var policy = new AuthorizationPolicyBuilder()
+//        .RequireAuthenticatedUser()
+//        .Build();
+//    options.Filters.Add(new AuthorizeFilter(policy));
+//});
+builder.Services.AddServerSideBlazor();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,6 +42,9 @@ app.UseRouting();
 app.UseAuthorization();
 //app.UseMiddleware<Terprint_3.Pages.FilterChooser>();
 app.MapRazorPages();
+app.MapBlazorHub();
+
+//app.MapRazorComponents<App>()    .AddInteractiveServerRenderMode();
 //app.MapRazorComponents<Terprint_3.Component.App>() ;
 
 app.Run();
