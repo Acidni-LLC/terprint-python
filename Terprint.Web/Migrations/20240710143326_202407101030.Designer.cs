199ac2d0e3464a680e2328791bd4a0b1c84a750a
Jamieson Gill <jgill@savitas.net>
2024-07-10 10:35:24 -0400

    add database componenents for login

--- Terprint.Web/Migrations/20240710143326_202407101030.Designer.cs
+++ Terprint.Web/Migrations/20240710143326_202407101030.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Terprint.Web.Data;
 
 #nullable disable
 
-namespace Terprint.Web.Migrations
+namespace Terprint.Web.Migrations.TerprintWeb
 {
     [DbContext(typeof(TerprintWebContext))]
-    partial class TerprintWebContextModelSnapshot : ModelSnapshot
+    [Migration("20240710143326_202407101030")]
+    partial class _202407101030
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
