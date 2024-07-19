d2703b0f51ddac69ecf5a8f51ee14dc2e9019571
savitas1 <jgill@savitas.net>
2024-07-19 17:05:45 -0400

    #29

--- Terprint.Web/SQLQuery2.sql
+++ Terprint.Web/SQLQuery2.sql
@@ -1,1 +1,7 @@
 ﻿select * from  RatingCategory
+
+select * from batch where name ='4281 4009 89411502'
+
+select * from thcvalues where  thcvalueid in (58,59,60,61,62,63,64,65,66,67,68)
+
+delete from thcvalues where thcvalueid in (58,59,60,61,62,63,64,65,66,67,68)
