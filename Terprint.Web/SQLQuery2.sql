7bb8d193f7befa0797af9b2be0a9cc1361ab862b
savitas1 <jgill@savitas.net>
2024-07-25 11:33:18 -0400

    #22 #21

--- Terprint.Web/SQLQuery2.sql
+++ Terprint.Web/SQLQuery2.sql
@@ -11,12 +11,13 @@ select s.StrainName,b.* from batch b join strain s on b.StrainID=s.StrainId wher
 select * from THCValues where batchid = (select batchid from batch where name ='1223298389111661') 
 select * from THCValues where [Percent] = 0
 select * from TerpeneValues where Value = 0
+select tv.*,b.Name from TerpeneValues tv join batch b on tv.BatchID=b.BatchId where tv.TerpeneName = ''
 --delete from THCValues where [Percent] = 0
 --delete from thcvalues where batchid = 87 in (58,59,60,61,62,63,64,65,66,67,68)
 
 select tv.TerpeneName,count(tv.TerpeneName) from TerpeneValues tv group by tv.TerpeneName order by count(tv.TerpeneName )  desc
 
-select t.Analyte, t.BatchID ,count(t.Analyte) from THCValues t group by t.Analyte, t.BatchID order by count(t.Analyte)  desc
+select t.Analyte, count(t.Analyte) from THCValues t group by t.Analyte  order by count(t.Analyte)  desc
  
 select b.Name,t.* from THCValues   t join batch b on t.BatchID = b.BatchId where t.analyte = 'Delta-'
 select b.Name, t.* from THCValues  t join batch b on t.BatchID = b.BatchId where t.LOD like '%THC%'
@@ -24,3 +25,4 @@ select t.Analyte, t.BatchID ,count(t.Analyte) from THCValues t group by t.Analyt
 select t.* from THCValues t where t.BatchID = '44' 
 
 select b.name,t.* from THCValues t join batch b on t.BatchID = b.BatchId where t.BatchID in (44,51,42,43,50,74,75,76,77,78,79,80,87) 
+update TerpeneValues set TerpeneName = '3-CARENE' where TerpeneValueId=549
