b19e5b520266df0305ee8cb4ec0a299f288b8af6
savitas1 <jgill@savitas.net>
2025-05-14 19:56:37 -0400

    syntax error fix

--- Terprint.Web/SQLQuery2.sql
+++ Terprint.Web/SQLQuery2.sql
@@ -6,7 +6,7 @@ select * from TerpeneValues where  lower(terpenename) like '%caryophyllene%' ord
 select top(100)* from thcvalues order by created desc
 select top(100)* from thcvalues order by batchid desc
 --delete from thcvalues where thcvalueid >= 789
-select s.StrainName,b.* from batch b join strain s on b.StrainID=s.StrainId where b.BatchId not in (Select BatchId from THCValues)
+select s.StrainName,b.* from batch b join strain s on b.StrainID=s.StrainId where b.BatchId not in (Select BatchId from THCValues) 
 
 select * from THCValues where batchid = (select batchid from batch where name ='18347_0004 808376') 
 select * from THCValues where [Percent] = 0
@@ -16,7 +16,7 @@ select tv.*,b.Name from TerpeneValues tv join batch b on tv.BatchID=b.BatchId wh
 --delete from THCValues where [batchid] = 87
 --delete from thcvalues where batchid = 87 in (58,59,60,61,62,63,64,65,66,67,68)
 
-select lower(tv.TerpeneName),count(tv.TerpeneName) from TerpeneValues tv group by tv.TerpeneName order by count(tv.TerpeneName 
+select lower(tv.TerpeneName),count(tv.TerpeneName) from TerpeneValues tv group by tv.TerpeneName order by count(tv.TerpeneName )
 
 select t.Analyte, count(t.Analyte) from THCValues t group by t.Analyte  order by count(t.Analyte)  desc
  
