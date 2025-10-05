0761cb143ca8823f014885d8e9a0a92d3eb79530
savitas1 <jgill@savitas.net>
2025-10-05 14:20:54 -0400

    Refactor SQL execution and result handling in sqlConnection.py

--- test_output/sqlConnection.py
+++ test_output/sqlConnection.py
@@ -10,7 +10,9 @@ from pyodbc import connect
 try:
     load_dotenv()
     #conn = connect(getenv("SQL_CONNECTION_STRING"))
+    #AZURE_SQL_CONNECTIONSTRING="Driver={ODBC Driver 18 for SQL Server};Server=tcp:acidni-sql.database.windows.net,1433;Database=terprint;Uid=jgill@acidnillc.onmicrosoft.com;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;Authentication=ActiveDirectoryInteractive"
     AZURE_SQL_CONNECTIONSTRING="Driver={ODBC Driver 18 for SQL Server};Server=tcp:acidni-sql.database.windows.net,1433;Database=terprint;Uid=jgill@acidnillc.onmicrosoft.com;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;Authentication=ActiveDirectoryInteractive"
+    
 #AZURE_SQL_CONNECTIONSTRING="Driver={ODBC Driver 18 for SQL Server};Server=tcp:acidni-sql.database.windows.net,1433;Database=terprint;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30"
 #connection_string = AZURE_SQL_CONNECTIONSTRING
     #AZURE_SQL_CONNECTIONSTRING="Driver={ODBC Driver 17 for SQL Server};Server=tcp:acidni-sql.database.windows.net,1433;Database=terprint;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30"
@@ -20,19 +22,52 @@ try:
 
     #You can now execute SQL queries using the cursor
     print("terpeneResults")
-
+#Terprintpw13579$
     cursor.execute("SELECT * FROM terpeneResults")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
-    cursor.execute("        INSERT INTO [dbo].[cannabinoidResults]            ([batch], [Index], [Cannabinoid], [percent], [milligrams],   [dispensaryId], [createdBy])        VALUES  ('1111' ,1 ,'THC' ,.025 ,.0025 ,1 ,'jgill@acidnillc.onmicrosoft.com')    ")
+    SQL_STATEMENT = """
+    INSERT INTO [dbo].[cannabinoidResults]  (
+    [batch], 
+    [Index], 
+    [Cannabinoid],
+    [percent],
+    [milligrams],  
+    [dispensaryId], 
+    [createdBy] ,
+    [created]
+    ) OUTPUT INSERTED.cannabinoidResultId
+    VALUES (?, ?, ?, ?,?,?, ?,CURRENT_TIMESTAMP)
+    """
+
+    cursor.execute(
+    SQL_STATEMENT,
+    (
+        '1111' ,
+        1 ,
+        'THC' ,
+        .025 ,
+        .0025 ,
+        1 ,
+        'jgill@acidnillc.onmicrosoft.com'
+    )
+    )
+    resultId = cursor.fetchval()
+    print(f"Inserted cannabinoidResultId : {resultId}")
+    conn.commit()
+
+    
 
     print("cannabinoidResults")
     cursor.execute("SELECT * FROM cannabinoidResults")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+
+    cursor.close()
+    conn.close()
 
 except pyodbc.Error as ex:
     sqlstate = ex.args[0]
