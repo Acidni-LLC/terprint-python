a32c1baa3bb41bdb487d569a90059fd3a8763719
savitas1 <jgill@savitas.net>
2025-10-05 13:12:58 -0400

    python sql working

---
+++ test_output/sqlConnection.py
@@ -0,0 +1,33 @@
+"""
+Connects to a SQL database using pyodbc
+"""
+from os import getenv
+from dotenv import load_dotenv
+import pyodbc
+from pyodbc import connect
+
+
+try:
+    load_dotenv()
+    #conn = connect(getenv("SQL_CONNECTION_STRING"))
+    AZURE_SQL_CONNECTIONSTRING="Driver={ODBC Driver 18 for SQL Server};Server=tcp:acidni-sql.database.windows.net,1433;Database=terprint;Uid=jgill@acidnillc.onmicrosoft.com;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;Authentication=ActiveDirectoryInteractive"
+#AZURE_SQL_CONNECTIONSTRING="Driver={ODBC Driver 18 for SQL Server};Server=tcp:acidni-sql.database.windows.net,1433;Database=terprint;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30"
+#connection_string = AZURE_SQL_CONNECTIONSTRING
+    #AZURE_SQL_CONNECTIONSTRING="Driver={ODBC Driver 17 for SQL Server};Server=tcp:acidni-sql.database.windows.net,1433;Database=terprint;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30"
+    conn = pyodbc.connect(AZURE_SQL_CONNECTIONSTRING)
+    cursor = conn.cursor()
+    print("Connection successful!")
+
+    # You can now execute SQL queries using the cursor
+    # Example: cursor.execute("SELECT * FROM YourTable")
+    # rows = cursor.fetchall()
+    # for row in rows:
+    #     print(row)
+
+except pyodbc.Error as ex:
+    sqlstate = ex.args[0]
+    print(f"Error connecting to SQL Server: {sqlstate}")
+
+finally:
+    if 'cnxn' in locals() and cnxn:
+        cnxn.close()
