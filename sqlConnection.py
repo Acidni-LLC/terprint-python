4b78a3239daac4dade6aea45145bd11f889161a7
savitas1 <jgill@savitas.net>
2025-10-09 14:39:30 -0400

    working good

---
+++ sqlConnection.py
@@ -0,0 +1,142 @@
+"""
+Connects to a SQL database using pyodbc
+"""
+from os import getenv
+from dotenv import load_dotenv
+import pyodbc
+from pyodbc import connect
+AZURE_SQL_CONNECTIONSTRING="Driver={ODBC Driver 18 for SQL Server};Server=tcp:acidni-sql.database.windows.net,1433;Database=terprint;Uid=adm;Pwd=sql1234%;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+      
+def insertcannabinoids( 
+        batch, 
+        Index, 
+        Cannabinoid,
+        percent,
+        milligrams,  
+        dispensaryId, 
+        createdBy):
+    try:
+        load_dotenv()
+        conn = pyodbc.connect(AZURE_SQL_CONNECTIONSTRING)
+        cursor = conn.cursor()
+       # print("Connection successful!")
+
+        #You can now execute SQL queries using the cursor
+  
+
+        SQL_STATEMENT = """
+        INSERT INTO [dbo].[cannabinoidResults]  (
+        [batch], 
+        [Index], 
+        [Cannabinoid],
+        [percent],
+        [milligrams],  
+        [dispensaryId], 
+        [createdBy] ,
+        [created]
+        ) OUTPUT INSERTED.cannabinoidResultId
+        VALUES (?, ?, ?, ?,?,?, ?,CURRENT_TIMESTAMP)
+        """
+        
+       # print(SQL_STATEMENT)    
+        print (str(Index) +"---"+batch + "," +        str(Index)  + "," +        Cannabinoid + "," +        percent + "," +        milligrams + "," +          str(dispensaryId) + "," +        createdBy+"---")
+        cursor.execute(
+        SQL_STATEMENT,
+        (
+             batch, 
+        Index, 
+        Cannabinoid,
+        percent,
+        milligrams,  
+        dispensaryId, 
+        createdBy
+        )
+        )
+        resultId = cursor.fetchval()
+        print(f"Inserted cannabinoidResultId : {resultId}")
+        conn.commit()
+
+        
+
+        # print("cannabinoidResults")
+        # cursor.execute("SELECT * FROM cannabinoidResults")
+        # rows = cursor.fetchall()
+        # for row in rows:
+        #     print(row)
+
+       # cursor.close()
+        #conn.close()
+
+    except pyodbc.Error as ex:
+        sqlstate = ex.args[0]
+        print(f"Error connecting to SQL Server: {sqlstate}")
+
+    finally:
+        if 'conn' in locals() and conn:
+            conn.close()
+
+def insertterpenes(
+    batch           ,
+    Index           ,
+    terpene           ,
+    percent           ,
+    milligrams           ,
+    dispensaryId        ,
+    createdBy):
+        
+    try:
+        load_dotenv()
+        conn = pyodbc.connect(AZURE_SQL_CONNECTIONSTRING)
+        cursor = conn.cursor()
+       # print("Connection successful!")
+
+        #You can now execute SQL queries using the cursor
+       # print("terpeneResults")
+    #Terprintpw13579$
+        # cursor.execute("SELECT * FROM terpeneResults")
+        # rows = cursor.fetchall()
+        # for row in rows:
+        #     print(row)
+
+        SQL_STATEMENT = """
+        INSERT INTO [dbo].[terpeneResults]
+           ([batch]
+           ,[Index]
+           ,[terpene]
+           ,[percent]
+           ,[milligrams]
+           ,[created]
+           ,[dispensaryId]
+        ,[createdBy])
+        OUTPUT INSERTED.terpeneResultId
+        VALUES (?, ?, ?, ?,?,CURRENT_TIMESTAMP,?, ?)
+        """
+
+        cursor.execute(
+        SQL_STATEMENT,
+        (
+           batch           ,Index           ,terpene           ,percent           ,milligrams           ,dispensaryId        ,createdBy
+        )
+        )
+        resultId = cursor.fetchval()
+        print(f"Inserted terpeneResultId : {resultId}")
+        conn.commit()
+
+        
+
+        # print("terpeneResults")
+        # cursor.execute("SELECT * FROM terpeneResults")
+        # rows = cursor.fetchall()
+        # for row in rows:
+        #     print(row)
+
+       #cursor.close()
+        #conn.close()
+
+    except pyodbc.Error as ex:
+        sqlstate = ex.args[0]
+        print(f"Error connecting to SQL Server: {sqlstate}")
+
+    finally:
+        if 'conn' in locals() and conn:
+            conn.close()
