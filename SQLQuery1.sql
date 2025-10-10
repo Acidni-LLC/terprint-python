b4ac9c334d8abfed07dfa7c1bc485328a2a20c25
savitas1 <jgill@savitas.net>
2025-10-09 21:01:50 -0400

    Refactor SQL queries for better clarity and consistency in SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,17 +6,32 @@
   FROM vw_cannabinoidResults
   SELECT distinct count(batch)
   FROM vw_terpeneResults
+  
+  SELECT *
+  FROM vw_terpeneResults
+ where batch not in (select batch from vw_cannabinoidResults)
+  order by batch,[index]
+  SELECT *
+  FROM vw_cannabinoidResults
+ where batch not in (select batch from vw_terpeneResults)
+  order by batch,[index]
+  
+  SELECT distinct Cannabinoid
+  FROM vw_cannabinoidResults
+ --where batch ='57901_0007452411'
+  order by Cannabinoid
+
 
   SELECT *
   FROM vw_cannabinoidResults
- where batch ='57901_0007452411'
-  order by batch
+ --where batch ='57901_0007452411'
+  order by batch,[index]
   
 
   SELECT *
   FROM vw_terpeneResults
- where batch ='57901_0007452411'
-  order by batch
+ --where batch ='57901_0007452411'
+  order by batch,[index]
 
   /*
   delete from terpeneResults
